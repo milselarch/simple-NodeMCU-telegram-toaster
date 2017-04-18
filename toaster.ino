a45e091a2e08416f4ece643086e471a5bee192f7
milselarch <milselarch@gmail.com>
2017-04-18 19:47:49 +0800

    used WifiManager libary instead of static ip config

--- toaster.ino
+++ toaster.ino
@@ -8,10 +8,17 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ESP8266TelegramBOT.h>
+#include <Servo.h>
 
-// Initialize Wifi connection to the router
-const char ssid[] = "Singtel7002-FBEF";              // your network SSID (name)
-const char password[] = "0013499375";                // your network key
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+const int toastDegree = 0;
+const int untoastDegree = 90;
+
+WiFiManager wifiManager;
+Servo myservo;
 
 // Initialize Telegram BOT
 #define BOTtoken "YOUR_BOT_TOKEN"  //token of TestBOT
@@ -21,40 +28,27 @@ TelegramBOT bot(BOTtoken, BOTname, YOUR_BOT_USERNAME);
 
 int Bot_mtbs = 1000; //mean time between scan messages
 long Bot_lasttime;   //last time messages' scan has been done
-
-/********************************************
- * EchoMessages - function to Echo messages *
- ********************************************/
- 
-void Bot_ReplyMessages() {
-    Serial.println(">>>telegram>>>");
-    for (int i = 1; i < bot.message[0][0].toInt() + 1; i++) {
-        String chatID = bot.message[i][4];
-        String message = bot.message[i][5];
-
-        String reply = "you said: " + message;
-        bot.sendMessage(bot.message[i][4], reply, "");
-        //chat_id message
-    }
-    Serial.println("<<<telegram<<<");
-    bot.message[0][0] = "";   // All messages have been replied - reset new messages
-}
-
+int degree = toastDegree;
 
 void setup() {
-    pinMode(D7, OUTPUT);
-    Serial.begin(115200);
+    //pinMode(D1, OUTPUT);
+    Serial.begin(57600);
     delay(3000);
+
+    myservo.attach(D1);
     
     // attempt to connect to Wifi network:
     Serial.print("Connecting Wifi: ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
+    wifiManager.autoConnect();
+    
+    /*
+    // Wifi.status() is never WL_CONNECTED for some reason
+    // even after it is connected to Wifi
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
+    */
 
     Serial.println("");
     Serial.println("WiFi connected");
@@ -64,17 +58,59 @@ void setup() {
     
     bot.begin();      // launch Bot functionalities
 }
+ 
+void Bot_ReplyMessages() {
+    Serial.println(">>>telegram>>>");
+    
+    for (int i = 1; i < bot.message[0][0].toInt() + 1; i++) {
+        String chatID = bot.message[i][4];
+        String message = bot.message[i][5];
+        String reply;
+
+        if (message == "/toast") {
+            degree = toastDegree;
+            reply = "toasting!";
+            //turn();
+            Serial.println("TOAST");
+            
+        } else if (message == "/untoast") {
+            degree = untoastDegree;
+            reply = "stopped toasting!";
+            //turn();
+            Serial.println("UNTOAST");
+            
+        } else {
+            reply = "command unknown, try /toast or /untoast";
+            Serial.println("UNKNOWN");
+        }
+
+        bot.sendMessage(bot.message[i][4], reply, "");
+        //chat_id message
+    }
+    
+    Serial.println("<<<telegram<<<");
+    bot.message[0][0] = "";   // All messages have been replied - reset new messages
+}
+
+long counts = 0;
 
 void loop() {
-    if (millis() > Bot_lasttime + Bot_mtbs)  {
-        digitalWrite(D7, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(100);              // wait for a second
-        digitalWrite(D7, LOW);    // turn the LED off by making the voltage LOW
-        
+    if (millis() > Bot_lasttime + Bot_mtbs) {
         bot.getUpdates(bot.message[0][1]);   // launch API GetUpdates up to xxx message
         Bot_ReplyMessages();
-        //Bot_EchoMessages();   // reply to message with Echo
+        //Bot_EchoMessages();                // reply to message with Echo
         Bot_lasttime = millis();
+        Serial.println(degree);
+        
+        Serial.print("COUNTS: ");
+        Serial.println(counts);
     }
+
+    myservo.write(degree);
+    delay(30);
+    counts++;
 }
+
+
+
 
